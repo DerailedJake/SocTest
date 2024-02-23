74304086bd888ba801a257efd049a82812f70b15
DerailedJake <piotrowskiror@gmail.com>
2024-02-24 00:11:07 +0100

    Image handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys:  [:username, :email, :avatar])
   end
 
 end
