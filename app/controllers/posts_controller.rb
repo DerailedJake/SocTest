62c252f61a6b28f314ece6f569f5a3f95ab430b6
DerailedJake <piotrowskiror@gmail.com>
2024-02-23 02:21:39 +0100

    Post creation, basic display, flashes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,23 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to root_path
+    else
+      flash[:danger] = @post.errors.full_messages.first
+      redirect_to new_post_path
+    end
   end
 
   def edit
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+
 end
