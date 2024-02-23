d881e9c0517b3038fc8400b9c2726cbfcc47efd7
DerailedJake <piotrowskiror@gmail.com>
2024-02-24 00:42:15 +0100

    Post images display

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :body, presence: true, length: { minimum: 10}
+  validates :body, presence: true, length: { minimum: 10 }
+  has_one_attached :picture
 end
