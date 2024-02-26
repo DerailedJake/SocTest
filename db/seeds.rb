bc15ed9a755700f9be0ecbc2b90d7f0198d8cd5f
DerailedJake <piotrowskiror@gmail.com>
2024-02-26 03:13:14 +0100

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,55 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+def create_posts(user)
+  min = 6
+  max = 40
+  rand(min..max).times do
+    user.posts.create(
+      body: Faker::Lorem.sentence(word_count: 10)
+    )
+  end
+end
+
+def create_stories(user)
+  min = 4
+  max = 12
+  rand(min..max).times do
+    user.stories.create(
+      title: Faker::Lorem.sentence(word_count: 3),
+      description: Faker::Lorem.sentence(word_count: 10)
+    )
+  end
+end
+
+def randomly_attach(user)
+  posts = user.posts
+  stories = user.story_ids
+  stories << nil
+  posts.each do |post|
+    post.update(story_ids: stories.sample)
+  end
+end
+
+user = User.create(
+  email: 'asdf@asdf.asdf',
+  password: 'qwerqwer',
+  password_confirmation: 'qwerqwer',
+  avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/factories/files/test_pic.jpeg", 'jpeg')
+)
+
+create_posts(user)
+create_stories(user)
+randomly_attach(user)
+
+20.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    password: 'qwerqwer',
+    password_confirmation: 'qwerqwer',
+    avatar: Rack::Test::UploadedFile.new("#{Rails.root}/spec/factories/files/test_pic.jpeg", 'jpeg')
+  )
+  create_posts(user)
+  create_stories(user)
+  randomly_attach(user)
+end
