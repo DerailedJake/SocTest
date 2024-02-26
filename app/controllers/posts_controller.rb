a12723f60a39c7d4ef26ae1d872d1d4feced27a1
DerailedJake <piotrowskiror@gmail.com>
2024-02-26 16:46:01 +0100

    Stories - button for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,20 @@
 class PostsController < ApplicationController
   def new
-    @post = Post.new
+    p params
+    @post = params[:post] ? Post.new(post_params) : Post.new
+    @story_to_redirect = params[:story_to_redirect]
   end
 
   def create
     @post = current_user.posts.new(post_params)
+    @story_to_redirect = params[:post][:story_to_redirect]
     if @post.save
       flash[:success] = "Post created!"
-      redirect_to root_path
+      if @story_to_redirect
+        redirect_to story_path(current_user.stories.find(@story_to_redirect))
+      else
+        redirect_to root_path
+      end
     else
       flash[:danger] = @post.errors.full_messages.first
       render 'new', status: 422
@@ -20,7 +27,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:picture, :body)
+    params.require(:post).permit(:picture, :body, { story_ids: []})
   end
 
 end
