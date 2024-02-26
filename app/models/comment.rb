720bf9b8e913b6d4e47eff7c9c918cfd9e2bcae0
DerailedJake <piotrowskiror@gmail.com>
2024-02-27 00:58:52 +0100

    KAN-63 controller and routes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  validates :body, presence: true, length: { maximum: 150 }
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   resources :stories
 
+  resources :comments, only: [:create, :update, :destroy]
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
