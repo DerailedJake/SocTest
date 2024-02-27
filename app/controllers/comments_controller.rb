c815e6ba7dbfcc8cae591dbb738703b7197872b2
DerailedJake <piotrowskiror@gmail.com>
2024-02-27 03:16:03 +0100

    KAN-58 comments ajax create and display

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,16 @@
 class CommentsController < ApplicationController
   def create
-
+    @comment = current_user.comments.new(comment_params)
+    respond_to do |format|
+      if @comment.save
+        flash[:success] = "Comment created!"
+        format.js   {}
+        format.json { render json: @comment, status: :created, location: @comment }
+      else
+        flash[:danger] = @comment.errors.full_messages.first
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
@@ -9,5 +19,11 @@ class CommentsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
   end
 end
