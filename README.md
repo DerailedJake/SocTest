aa17e90714aa6cfa12cbe5020a46bae229089a95
DerailedJake <piotrowskiror@gmail.com>
2024-02-28 15:15:23 +0100

    Readme, move mechanize_pic to lib

--- README.md
+++ README.md
@@ -1,24 +1,14 @@
-# README
+# Project #1
+# LifeBook
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+#### I'm currently ~50% done with first phase. 
 
-Things you may want to cover:
+#### Second phase are mostly improvements, optimization.
 
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+About the project:
+* User accounts with devise, avatar attachement
+* Users can create posts with description and optional picture
+* Users can create stories and attach posts to them
+* Tests are currently basic, often incomplete, will be expanded in following days
+* Mechanize used to scrape pictures from internet
+* ChatGPT used to create lots of text on website, posts and users descriptions 
---
+++ lib/mechanize_pic.rb
@@ -0,0 +1,44 @@
+
+fuck_off_from_this_code = true
+
+unless fuck_off_from_this_code
+  require 'rubygems'
+  require 'mechanize'
+  agent = Mechanize.new
+
+  website_pages = [
+    'https://www.archdaily.com/category/landscape-architecture',
+    'https://www.archdaily.com/category/landscape-architecture/page/2',
+    'https://www.archdaily.com/category/landscape-architecture/page/3',
+    'https://www.archdaily.com/category/landscape-architecture/page/4'
+  ]
+
+  image_count = 1
+
+  website_pages.each do |single_page|
+    page = agent.get(single_page)
+
+    images = page.search(".afd-search-list__img")
+
+    images_links = images.map{|i| i.attribute_nodes[1].value }
+
+    images_links.map! { |s| s.sub('small_jpg', 'newsletter') }
+
+    titles_raw = page.search(".afd-search-list__title")
+
+    p images.count
+
+    p titles_raw.count
+
+    images.count.times do |i|
+      p 'kurwa :)'
+      sleep 0.5
+      titles = titles_raw.map { |t| t.children.first.text.strip }
+      titles.map! { |t| t.split('/')[0].strip }
+      title = titles[i - 1].gsub(/\s+/, "_")
+
+      agent.get(images_links[i - 1]).save "app/assets/images/post_pictures/#{title}_#{image_count}_pic.jpeg"
+      image_count += 1
+    end
+  end
+end
