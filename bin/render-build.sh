ed0ba5fbd9ddac03d04083541a82060ad865976c
DerailedJake <piotrowskiror@gmail.com>
2024-02-28 03:20:29 +0100

    Goddammit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,11 @@
 set -o errexit
 
 bundle install
+rails db:drop
 rails db:drop:_unsafe
-rails db:reset
+rails db:drop
+rails db:create
+rails db:migrate
 rails db:seed
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
