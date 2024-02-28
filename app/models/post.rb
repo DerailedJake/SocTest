f80893f6f58d113049df5d4873afa06c6e6b990f
DerailedJake <piotrowskiror@gmail.com>
2024-02-28 15:55:11 +0100

    Fix conflicts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_and_belongs_to_many :stories
   validates :body, presence: true, length: { minimum: 10 }
   has_one_attached :picture
+  has_many :comments, dependent: :destroy
   def short_description
     body.truncate(30)
   end
