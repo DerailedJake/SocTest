931dc169e73487015701abf7c5c1a4b3339c9571
DerailedJake <piotrowskiror@gmail.com>
2024-03-01 00:57:51 +0100

    Expand seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,16 +14,26 @@ def user_descriptions
   @all_user_descriptions.sample
 end
 
-@all_post_descriptions = File.readlines("#{Rails.root}/app/assets/text_files/post_descriptions.txt").map! { |l| l.strip }
+@pos_post_descriptions = File.readlines("#{Rails.root}/app/assets/text_files/post_descriptions.txt").map! { |l| l.strip }
+@neg_post_descriptions = File.readlines("#{Rails.root}/app/assets/text_files/p_desc_neg.txt").map! { |l| l.strip }
 
-def post_descriptions
-  @all_post_descriptions.sample
+def pos_post_descriptions
+  @pos_post_descriptions.sample
 end
 
-@all_post_comments = File.readlines("#{Rails.root}/app/assets/text_files/post_comments.txt").map! { |l| l.strip }
+def neg_post_descriptions
+  @neg_post_descriptions.sample
+end
 
-def post_comment
-  @all_post_comments.sample
+@pos_post_comments = File.readlines("#{Rails.root}/app/assets/text_files/post_comments.txt").map! { |l| l.strip }
+@neg_post_comments = File.readlines("#{Rails.root}/app/assets/text_files/p_com_neg.txt").map! { |l| l.strip }
+
+def pos_post_comment
+  @pos_post_comments.sample
+end
+
+def neg_post_comment
+  @neg_post_comments.sample
 end
 
 def random_avatar
@@ -46,36 +56,41 @@ def create_stories(user)
     title = random_picture.original_filename.split('/')[-1].split('_')[0..-3].join(' ')
     user.stories.create!(
       title: title,
-      description: post_descriptions
+      description: pos_post_descriptions
     )
   end
 end
 
 def create_posts(user)
-  min = 24
-  max = 60
-  rand(min..max).times do
+  post_count = rand(24..60)
+  p "number of posts - #{post_count}"
+  post_count.times do
+    if rand(1..10) > 3
+      p_body = pos_post_descriptions
+      is_positive = true
+    else
+      p_body = neg_post_descriptions
+      is_positive = false
+    end
     picture = random_picture
     title = random_picture.original_filename.split('/')[-1].split('_')[0..-3].join(' ')
-    user.posts.create!(
-      body: "#{title} #{post_descriptions}",
+    post = user.posts.create!(
+      body: "#{title} #{p_body}",
       picture: picture
     )
+    create_comments(post, is_positive)
   end
 end
 
-def create_comments
-  @posts = Post.all
-  @users = User.all
-  @posts.each do |post|
-    min = 0
-    max = 15
-    rand(min..max).times do
-      post.comments.create!(
-        body: post_comment,
-        user: @users.sample
-      )
-    end
+def create_comments(post, is_positive)
+  min = 0
+  max = 15
+  rand(min..max).times do
+    c_body = is_positive ? pos_post_comment : neg_post_comment
+    post.comments.create!(
+      body: c_body,
+      user: @all_users.sample
+    )
   end
 end
 
@@ -88,7 +103,21 @@ def randomly_attach(user)
   end
 end
 
-user = User.create!(
+def create_users
+  rand(50..80).times do
+    User.create!(
+      email: Faker::Internet.email,
+      password: 'qwerqwer',
+      password_confirmation: 'qwerqwer',
+      avatar: random_avatar,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      description: user_descriptions
+    )
+  end
+end
+
+User.create!(
   email: 'asdf@asdf.asdf',
   password: 'qwerqwer',
   password_confirmation: 'qwerqwer',
@@ -98,23 +127,14 @@ user = User.create!(
   description: user_descriptions
 )
 
-create_posts(user)
-create_stories(user)
-randomly_attach(user)
+create_users
 
-40.times do
-  user = User.create!(
-    email: Faker::Internet.email,
-    password: 'qwerqwer',
-    password_confirmation: 'qwerqwer',
-    avatar: random_avatar,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    description: user_descriptions
-  )
+@all_users = User.all
+
+# comments made during post creation
+@all_users.each do |user|
+  p "starting - #{user.first_name}"
   create_posts(user)
   create_stories(user)
   randomly_attach(user)
 end
-
-create_comments
