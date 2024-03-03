1cb2730160e15e0029151f3321f6a500f9d67706
DerailedJake <piotrowskiror@gmail.com>
2024-03-04 00:36:58 +0100

    Fix specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    #first_name { Faker::Name.first_name }
-    #last_name  { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    description { Faker::Lorem.sentence(word_count: 3) }
     email { Faker::Internet.email }
     avatar { Rack::Test::UploadedFile.new("#{Rails.root}/app/assets/images/user_avatars/1.jpeg", 'jpeg') }
     password { Faker::Internet.password }
