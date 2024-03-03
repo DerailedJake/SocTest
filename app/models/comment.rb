788cfcf476c0c68860b8e3ca2aade28bb663dc62
DerailedJake <piotrowskiror@gmail.com>
2024-03-03 22:47:10 +0100

    KAN-105 deleting posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :body, presence: true, length: { maximum: 150 }
+  validates :body, presence: true, length: { minimum: 1, maximum: 150 }
   belongs_to :user
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :stories
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :body, presence: true, length: { minimum: 10, maximum: 300 }
   has_one_attached :picture
   has_many :comments, dependent: :destroy
 
