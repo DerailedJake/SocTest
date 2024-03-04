87760d6bda0541bd199dcdb342108bdd12e8cdba
DerailedJake <piotrowskiror@gmail.com>
2024-03-04 17:25:01 +0100

    KAN-111 system specs for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,17 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name  { Faker::Name.last_name }
-    description { Faker::Lorem.sentence(word_count: 3) }
+    description { Faker::Lorem.sentence(word_count: 12) }
     email { Faker::Internet.email }
     avatar { Rack::Test::UploadedFile.new("#{Rails.root}/app/assets/images/user_avatars/1.jpeg", 'jpeg') }
     password { Faker::Internet.password }
+  end
+end
+
+
+def user_with_posts_and_stories(object_count: 7)
+  FactoryBot.create(:user) do |user|
+    FactoryBot.create_list(:post, object_count, user: user)
+    FactoryBot.create_list(:story, object_count, user: user)
   end
 end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,175 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    @current_user = user_with_posts_and_stories
+  end
+
+  describe 'GET /' do
+    context 'when logged in' do
+      before do
+        login_as(@current_user)
+        @story = @current_user.stories.last
+        @post = @current_user.posts.last
+      end
+
+      it 'returns a success response' do
+        visit users_home_path
+        expect(page).to have_content(@current_user.full_name)
+        expect(page).to have_content(@story.title)
+        expect(page).to have_content(@post.body)
+      end
+
+      it 'has working pagination of stories' do
+        visit users_home_path
+        within('#story-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(@current_user.stories.first.title)
+      end
+
+      it 'has working pagination of posts' do
+        visit users_home_path
+        within('#post-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(@current_user.posts.first.body)
+      end
+    end
+
+    context 'without stories or posts' do
+      it 'returns a success response' do
+        login_as(@current_user)
+        @current_user.stories.destroy_all
+        @current_user.posts.destroy_all
+        visit users_home_path
+        expect(page).to have_content 'has no posts'
+        expect(page).to have_content 'has no stories'
+      end
+    end
+
+    context 'when logged out' do
+      it 'returns a success response' do
+        visit users_home_path
+        expect(page).to have_content 'Log in'
+      end
+    end
+  end
+
+  describe 'GET /users/profile' do
+    before do
+      @user = user_with_posts_and_stories
+      login_as(@current_user)
+      @story = @user.stories.last
+      @post = @user.posts.last
+    end
+
+    context 'when logged in' do
+      it 'returns a success response' do
+        visit profile_path(@user)
+        expect(page).to have_content(@user.full_name)
+        expect(page).to have_content(@story.title)
+        expect(page).to have_content(@post.body)
+      end
+
+      it 'has working pagination of stories' do
+        visit profile_path(@user)
+        within('#story-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(@user.stories.first.title)
+      end
+
+      it 'has working pagination of posts' do
+        visit profile_path(@user)
+        within('#post-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(@user.posts.first.body)
+      end
+    end
+
+    context 'without stories or posts' do
+      it 'returns a success response' do
+        login_as(@current_user)
+        @user.stories.destroy_all
+        @user.posts.destroy_all
+        visit profile_path(@user)
+        expect(page).to have_content 'has no posts'
+        expect(page).to have_content 'has no stories'
+      end
+    end
+
+    context 'when logged out' do
+      it 'returns a success response' do
+        visit profile_path(@user)
+        expect(page).to have_content(@user.full_name)
+        expect(page).to have_content(@story.title)
+        expect(page).to have_content(@post.body)
+      end
+    end
+  end
+
+  describe 'GET /users/index' do
+    context 'when logged in' do
+      before do
+        login_as @current_user
+        12.times { User.create!(attributes_for(:user)) }
+      end
+
+      it 'opens users list' do
+        visit users_index_path
+        expect(page).to have_content(User.first.full_name)
+        expect(page).to have_content(User.first.description)
+        expect(page).to have_content(User.third.full_name)
+        expect(page).to have_content(User.third.description)
+      end
+
+      it 'has working pagination of users' do
+        visit users_index_path
+        within('#user-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(User.last.full_name)
+        expect(page).to have_content(User.last.description)
+      end
+
+      it 'opens profile when clicked' do
+        visit users_index_path
+        within('#user-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(User.last.full_name)
+      end
+    end
+
+    context 'when logged out' do
+      before do
+        12.times { User.create!(attributes_for(:user)) }
+      end
+
+      it 'opens users list' do
+        visit users_index_path
+        expect(page).to have_content(User.first.full_name)
+        expect(page).to have_content(User.first.description)
+      end
+
+      it 'has working pagination of users' do
+        visit users_index_path
+        within('#user-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(User.last.full_name)
+        expect(page).to have_content(User.last.description)
+      end
+
+      it 'opens profile when clicked' do
+        visit users_index_path
+        within('#user-pagination') do
+          click_link('3')
+        end
+        expect(page).to have_content(User.last.full_name)
+      end
+    end
+  end
+end
