3a5bfa27ec4710f96c718eef608281ea1477f8f5
DerailedJake <piotrowskiror@gmail.com>
2024-03-05 15:18:12 +0100

    KAN-116 static pages system specs - shared examples, fixes

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,73 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :system do
+RSpec.shared_examples 'check static pages' do
+  describe 'GET /landing' do
+    it 'returns a success response' do
+      visit static_pages_landing_path
+      expect(page).to have_content 'Join Today'
+      expect(page).to have_content User.first.full_name
+    end
+  end
+
+  describe 'GET /about' do
+    it 'returns a success response' do
+      visit static_pages_about_path
+      expect(page).to have_content 'Information on github readme.'
+    end
+  end
+
+  describe 'GET /contact' do
+    it 'returns a success response' do
+      visit static_pages_contact_path
+      expect(page).to have_content 'Hey, thanks for stopping by.'
+    end
+  end
+
+  describe 'GET /legal' do
+    it 'returns a success response' do
+      visit static_pages_legal_path
+      expect(page).to have_content 'Legal Mumbo Jumbo'
+    end
+  end
+end
+
+RSpec.describe 'StaticPages', type: :system do
   before(:example) do
     @current_user = user_with_posts_and_stories
     2.times { create :user }
   end
 
-  context 'when loggen in' do
+  context 'when logged in' do
     before do
       login_as(@current_user)
     end
-    describe 'GET /landing' do
-      it 'returns a success response' do
-        visit static_pages_landing_path
-        expect(page).to have_content 'Join Today'
-        expect(page).to have_content User.first.full_name
-      end
-    end
-
-    describe 'GET /about' do
-      it 'returns a success response' do
-        visit static_pages_about_path
-        expect(page).to have_content 'Information on github readme.'
-      end
-    end
-
-    describe 'GET /contact' do
-      it 'returns a success response' do
-        visit static_pages_contact_path
-        expect(page).to have_content 'Hey, thanks for stopping by.'
-      end
-    end
-
-    describe 'GET /legal' do
-      it 'returns a success response' do
-        visit static_pages_legal_path
-        expect(page).to have_content 'Legal Mumbo Jumbo'
-      end
-    end
+    include_examples 'check static pages'
   end
 
-  context 'when loggen in' do
-    describe 'GET /landing' do
-      it 'returns a success response' do
-        visit static_pages_landing_path
-        expect(page).to have_content 'Join Today'
-        expect(page).to have_content User.first.full_name
-      end
-    end
-
-    describe 'GET /about' do
-      it 'returns a success response' do
-        visit static_pages_about_path
-        expect(page).to have_content 'Information on github readme.'
-      end
-    end
-
-    describe 'GET /contact' do
-      it 'returns a success response' do
-        visit static_pages_contact_path
-        expect(page).to have_content 'Hey, thanks for stopping by.'
-      end
-    end
-
-    describe 'GET /legal' do
-      it 'returns a success response' do
-        visit static_pages_legal_path
-        expect(page).to have_content 'Legal Mumbo Jumbo'
-      end
-    end
+  context 'when logged out' do
+    include_examples 'check static pages'
   end
 end
