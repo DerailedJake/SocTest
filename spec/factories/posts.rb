a1358c7359911cabc9a685aa045b2aa61e2a0bd2
DerailedJake <piotrowskiror@gmail.com>
2024-03-06 17:58:03 +0100

    KAN-116 static pages system specs - header links wip?

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :post do
     body { Faker::Lorem.sentence(word_count: 10) }
     picture { Rack::Test::UploadedFile.new("#{Rails.root}/app/assets/images/post_pictures/25_Pavilion_92_pic.jpeg", 'jpeg') }
+    user
   end
 end
--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -31,20 +31,85 @@ RSpec.shared_examples 'check static pages' do
   end
 end
 
-RSpec.describe 'StaticPages', type: :system do
-  before(:example) do
-    @current_user = user_with_posts_and_stories
-    2.times { create :user }
-  end
-
-  context 'when logged in' do
-    before do
-      login_as(@current_user)
+RSpec.shared_examples 'check header links' do
+  context 'LifeBook' do
+    it 'returns a success response' do
+      sleep(0.2)
+      within('.navbar') { click_on 'LifeBook' }
+      sleep(0.2)
+      if logged_in
+        expect(page).to have_current_path(root_path)
+      else
+        expect(page).to have_current_path(new_user_session_path)
+      end
     end
-    include_examples 'check static pages'
+  end
+  context 'Users' do
+    it 'returns a success response' do
+      within('.navbar') { click_on 'Users' }
+      sleep(0.2)
+      expect(page).to have_content('LifeBook Users')
+    end
+  end
+  context 'Stories' do
+    it 'returns a success response' do
+      within('.navbar') { click_on 'Stories' }
+      sleep(0.2)
+      expect(page).to have_content('Discover New Stories!')
+    end
+  end
+  context 'Landing' do
+    it 'returns a success response' do
+      within('.navbar') { click_on 'Landing' }
+      sleep(0.2)
+      expect(page).to have_content('Join Today')
+    end
+  end
+end
+
+RSpec.describe 'StaticPages', type: :system do
+  before do
+    @current_user = user_with_posts_and_stories
+    @second_user = user_with_posts_and_stories
+    @third_user = user_with_posts_and_stories
+  end
+  context 'pages' do
+    context 'when logged in' do
+      before do
+        login_as(@current_user)
+        sleep(0.1)
+      end
+      include_examples 'check static pages'
+    end
+
+    context 'when logged out' do
+      before do
+        sleep(0.1)
+      end
+      include_examples 'check static pages'
+    end
   end
 
-  context 'when logged out' do
-    include_examples 'check static pages'
+  context 'Header' do
+    context 'when logged in' do
+      before do
+        login_as(@current_user)
+        visit static_pages_about_path
+        sleep(0.1)
+      end
+      include_examples 'check header links' do
+        let(:logged_in) { true }
+      end
+    end
+
+    context 'when logged out' do
+      before do
+        visit static_pages_about_path
+        sleep(0.1)
+      end
+      include_examples 'check header links' do
+        let(:logged_in) { false }
+      end
+    end
   end
 end
