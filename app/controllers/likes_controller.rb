9ba0d652bf20f8c31f9d0faefa3e26d9001dcc86
DerailedJake <piotrowskiror@gmail.com>
2024-03-15 00:27:52 +0100

    KAN-54 improve liked display

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -17,17 +17,8 @@ class LikesController < ApplicationController
 
   def liked_things
     @thing = params[:thing]
-    @likes = current_user.likes.where(likeable_type: @thing)
-    # fix this later
-    # @likeables = current_user.likes.includes(:likeable).select{|l| l.likeable if l.likeable_type == 'Story'}
-    if @thing == 'Story'
-      @thing_name = 'Stories'
-      @likeables = Story.joins(:likes).where(likes: {user_id: current_user.id})
-    elsif @thing == 'Post'
-      @thing_name = 'Posts'
-      @likeables = Post.joins(:likes).where(likes: {user_id: current_user.id})
-    end
-    @pagy_likeable, @likeables = pagy(@likeables, items: 3, page_param: :page_stories)
+    @pagy_likeable, @likeables = pagy(current_user.likes.includes(:likeable).where(likeable_type: @thing), items: 3)
+    @likeables = @likeables.map(&:likeable)
   end
 
   private
