9e937e11f08c2f81163cfc3275eaf3546ac6b181
DerailedJake <piotrowskiror@gmail.com>
2024-03-14 02:57:02 +0100

    KAN-50, KAN-51 likes for posts and stories

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :stories
   has_many :comments, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
   has_one_attached :picture
   validates :body, presence: true, length: { minimum: 3, maximum: 500 }
   validate :stories_belong_to_user
