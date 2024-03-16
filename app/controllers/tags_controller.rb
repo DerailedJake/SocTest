231f9fd1ac9245d538d888ea0b6aaab90c95946e
DerailedJake <piotrowskiror@gmail.com>
2024-03-16 19:40:11 +0100

    Tweak tag display

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @tags = Tag.all.order('name ASC')
+    @tags = Tag.all.order('taggable_count DESC')
   end
 
   def show
