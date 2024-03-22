14e765aa28d01d2521519c1d6c31947c368dcadc
DerailedJake <piotrowskiror@gmail.com>
2024-03-22 15:02:54 +0100

    KAN-210 specs for side panel

--- spec/system/contacts_spec.rb
+++ spec/system/contacts_spec.rb
@@ -3,8 +3,73 @@ require 'rails_helper'
 RSpec.describe 'Contact', type: :system do
   before do
     @current_user = user_with_posts_and_stories
-    create_list(:user, 7)
-    @second_user = User.second
+    @users = create_list(:user, 7)
+    @second_user = @users.first
+  end
+
+  describe 'side panel' do
+    context 'open and collapse' do
+      before do
+        login_as @current_user
+        visit static_pages_about_path
+      end
+      it 'should work properly' do
+        expect(page).to have_content 'Contacts'
+        click_on 'Contacts'
+        within '#side-panel' do
+          expect(page).to have_content 'You have no contacts yet'
+        end
+        click_on 'Contacts'
+        expect(page).not_to have_selector '#side-panel'
+        expect(page).to have_current_path static_pages_about_path
+      end
+    end
+    context 'when logged IN' do
+      before do
+        login_as @current_user
+        visit static_pages_about_path
+      end
+      context 'with no people' do
+        it 'shows empty panel' do
+          expect(page).to have_content 'Contacts'
+          click_on 'Contacts'
+          within '#side-panel' do
+            expect(page).to have_content 'You have no contacts yet'
+          end
+        end
+      end
+      context 'with people' do
+        before do
+          @users.each do |user|
+            @current_user.contacts.create!(acquaintance: user)
+          end
+        end
+        it 'shows contacts' do
+          expect(page).to have_content 'Contacts'
+          click_on 'Contacts'
+          within '#side-panel' do
+            expect(page).to have_content @users.first.full_name
+            expect(page).to have_content @users.last.full_name
+          end
+        end
+        it 'has working links' do
+          expect(page).to have_content 'Contacts'
+          click_on 'Contacts'
+          within '#side-panel' do
+            expect(page).to have_content @second_user.full_name
+            click_on @second_user.full_name
+          end
+          expect(page).to have_current_path profile_path @second_user
+        end
+      end
+    end
+    context 'when logged OUT' do
+      it 'it does not show side panel' do
+        visit static_pages_about_path
+        expect(page).not_to have_content 'Contacts'
+        expect(page).not_to have_selector '#side-panel'
+      end
+    end
   end
 
   describe 'observing people' do
@@ -30,8 +95,7 @@ RSpec.describe 'Contact', type: :system do
       end
       context 'contacts index' do
         before do
-          User.all.each do |user|
-            next if user == @current_user
+          @users.each do |user|
             @current_user.contacts.create!(acquaintance: user)
           end
         end
