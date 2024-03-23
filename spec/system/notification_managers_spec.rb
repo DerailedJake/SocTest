d868bbf712a04e13bec3c7f927e3b56c5a7c124b
DerailedJake <piotrowskiror@gmail.com>
2024-03-23 23:57:46 +0100

    KAN-217 basic NotificationManager specs

---
+++ spec/system/notification_managers_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'NotificationManagers', type: :system do
+  before do
+    @current_user = user_with_posts_and_stories
+    login_as @current_user
+  end
+
+  describe 'change options' do
+    it 'renders settings page' do
+      visit notification_options_path
+      expect(page).to have_content 'Notification Options:'
+    end
+    it 'properly updates settings' do
+      visit notification_options_path
+      uncheck 'settings[story_commented]'
+      click_on 'Update options'
+      expect(page).to have_content 'Settings changed'
+    end
+  end
+end
